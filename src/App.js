2b59100353f83fb60a5596cd2181c674df4c9b5b
Anshika Prajapati <anshikaprajapati124@gmail.com>
2023-10-12 09:17:52 +0530

    singin updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/form.css'
 
-function App() {
+function app()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="mainBackground">
+        <div class="container">
+          <section class="firstSection">
+            <div class="formQuote">You look the most beautiful when you wear your smile!</div>
+          </section>
+          <section class="secondSection">
+            <form>
+              <div class="dataContainer">
+                <div class="formGroup">
+                  <label for="username">Name</label>
+                  <input type="text" id="username" name="username"/>
+                </div>
+                <div class="formGroup">
+                  <label for="age">Age</label>
+                  <input type="number" id="age" name="age"/>
+                </div>
+                <div class="formGroup">
+                  <label for="email">Email</label>
+                  <input type="email" id="email" name="email"/>
+                </div>
+                <div class="formGroup">
+                  <label for="number">Number</label>
+                  <input type="number" id="number" name="number"/>
+                </div>
+                <div class="formGroup">
+                  <label for="password">Password</label>
+                  <input type="password" id="password" name="password"/>
+                </div>
+              </div>
+              <div class="buttonContainer">
+                <button>Register</button>
+                <div class="accountSwitch">
+                  <p>Already Registred? <a href="#">Login</a></p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+
+      </div>
+    </>
   );
 }
 
-export default App;
+export default app;
